f4413fe0302ff3bfaace7620f8b2b55ce3c78339
Evgenii <emeleshkov@mail.ru>
2022-05-16 02:19:50 +0300

    test1

--- codeWars.js
+++ codeWars.js
@@ -137,3 +137,12 @@ function squareDigits(num){
 }
 
 //--------------------------------------------------------------------
+
+
+function defineSuit(card) {
+    // good luck
+    return card.includes('♣') ? 'clubs'
+        : card.includes('♦') ? 'diamonds'
+            : card.includes('♥') ? 'hearts'
+                : 'spades'
+}
